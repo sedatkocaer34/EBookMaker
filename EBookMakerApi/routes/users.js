a18faa1e087e04dd7190c6d03e5c195745078151
Sedat <sedatkocaer3734@hotmail.com>
2021-03-27 23:33:42 +0300

    Apply get,update method for user

--- EBookMakerApi/routes/users.js
+++ EBookMakerApi/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const mongoose =require('mongoose');
 /* GET users listing. */
 router.get('/', (req, res, next)=> {
   res.send('respond with a resource');
@@ -16,5 +17,36 @@ router.post('/addNewUser',(req,res,next) =>{
     res.json({data:null,error:err.message});
   });
 });
+
+router.get('/getUser/:userId',(req,res,next) =>{
+  const userId= req.params.userId;
+  console.log(userId);
+  const promise = User.findById(mongoose.Types.ObjectId(userId));
+
+  promise.then((user)=>{
+    res.json(user);
+  }).catch((err)=>{
+    res.json({user:null,error:err.message});
+  });
+});
+
+router.put('/updateUser/:userId',(req,res,next) =>{
+  const userId= req.params.userId;
+  const userUpdate = JSON.parse(JSON.stringify(req.body));
+  console.log(userUpdate);
+
+  const promise = User.findByIdAndUpdate(userId ,userUpdate,{new :true})
+
+   promise.then((user)=>{
+     if(!user)
+     {
+       res.json({message:"User not found"});
+     }
+     res.json(user);  
+   }).catch((err)=>{
+     res.json({user:null,error:err.message});
+   });
+});
+
 
 module.exports = router;
